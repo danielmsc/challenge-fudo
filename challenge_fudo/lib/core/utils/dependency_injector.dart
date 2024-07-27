55987c75dc2cccd7b31ad41c2a1ba5fcb7ab7c63
daniel <massaccisidaniel@gmail.com>
2024-07-27 18:51:07 -0300

    Inyección de dependencias login

--- challenge_fudo/lib/core/utils/dependency_injector.dart
+++ challenge_fudo/lib/core/utils/dependency_injector.dart
@@ -1,17 +1,43 @@
+import 'package:challenge_fudo/core/data/data_sources/local_data_source.dart';
 import 'package:challenge_fudo/core/data/network/dio_client.dart';
+import 'package:challenge_fudo/core/data/repositories/local_repository_impl.dart';
+import 'package:challenge_fudo/core/domain/repositories/local_repository.dart';
+import 'package:challenge_fudo/login/data/data_sources/login_data_source.dart';
+import 'package:challenge_fudo/login/data/repositories/login_repository_impl.dart';
+import 'package:challenge_fudo/login/domain/repositories/login_repository.dart';
 import 'package:challenge_fudo/posts/data/data_sources/posts_data_source.dart';
 import 'package:challenge_fudo/posts/data/repositories/posts_repository_impl.dart';
 import 'package:challenge_fudo/posts/domain/repositories/posts_repository.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DependencyInjector {
   static createPostsRepository() {
     try {
       return Get.find<PostsRepository>();
     } catch (_) {
-      final PostsDataSource dataSource =
+      final dataSource =
           Get.put(PostsDataSourceImpl(client: Get.find<DioClient>()));
       Get.put<PostsRepository>(PostsRepositoryImpl(dataSource: dataSource));
+    }
+  }
+
+  static createLoginRepository() {
+    try {
+      return Get.find<LoginRepository>();
+    } catch (_) {
+      final dataSource = Get.put(LoginDataSourceImpl());
+      Get.put<LoginRepository>(LoginRepositoryImpl(dataSource: dataSource));
+    }
+  }
+
+  static createLocalRepository() {
+    try {
+      return Get.find<LocalRepository>();
+    } catch (_) {
+      final dataSource =
+          Get.put(LocalDataSourceImpl(prefs: Get.find<SharedPreferences>()));
+      Get.put<LocalRepository>(LocalRepositoryImpl(dataSource: dataSource));
     }
   }
 }
--- challenge_fudo/lib/login/bindings/login_bindings.dart
+++ challenge_fudo/lib/login/bindings/login_bindings.dart
@@ -1,29 +1,17 @@
-import 'package:challenge_fudo/core/data/data_sources/local_data_source.dart';
-import 'package:challenge_fudo/core/data/repositories/local_repository_impl.dart';
-import 'package:challenge_fudo/core/domain/repositories/local_repository.dart';
-import 'package:challenge_fudo/login/data/data_sources/login_data_source.dart';
-import 'package:challenge_fudo/login/data/repositories/login_repository_impl.dart';
-import 'package:challenge_fudo/login/domain/repositories/login_repository.dart';
+import 'package:challenge_fudo/core/utils/dependency_injector.dart';
 import 'package:challenge_fudo/login/domain/use_cases/login.dart';
 import 'package:challenge_fudo/login/domain/use_cases/set_authentication_status.dart';
 import 'package:challenge_fudo/login/presentation/controllers/login_controller.dart';
 import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginBindings extends Bindings {
   @override
   void dependencies() {
-    final loginDataSource = Get.put<LoginDataSource>(LoginDataSourceImpl());
-    final loginRepository = Get.put<LoginRepository>(
-        LoginRepositoryImpl(dataSource: loginDataSource));
-    final loginUseCase = Login(repository: loginRepository);
+    final loginUseCase =
+        Login(repository: DependencyInjector.createLoginRepository());
 
-    final localDataSource = Get.put<LocalDataSource>(
-        LocalDataSourceImpl(prefs: Get.find<SharedPreferences>()));
-    final localRepository = Get.put<LocalRepository>(
-        LocalRepositoryImpl(dataSource: localDataSource));
-    final setAuthenticationStatusUseCase =
-        SetAuthenticationStatus(repository: localRepository);
+    final setAuthenticationStatusUseCase = SetAuthenticationStatus(
+        repository: DependencyInjector.createLocalRepository());
 
     Get.lazyPut<LoginController>(
       () => LoginController(
