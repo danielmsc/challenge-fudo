5d7dc6ecfba46d2a0fc1a521b29be31f1ca67422
daniel <massaccisidaniel@gmail.com>
2024-07-28 16:10:08 -0300

    Llamado de casos de uso desde el controller de posts

--- challenge_fudo/lib/posts/presentation/controllers/posts_controller.dart
+++ challenge_fudo/lib/posts/presentation/controllers/posts_controller.dart
@@ -36,9 +36,13 @@ class PostsController extends GetxController with StateMixin {
 
   Future<void> getPosts() async {
     final result = await getPostsUseCase(NoParamsGetPosts());
-    result.fold((l) => l.showError(), (r) {
+    result.fold((l) async {
+      l.showError();
+      await _getLocalPosts();
+    }, (r) async {
       posts = r;
       _filteredPosts.value = List.from(r);
+      await _insertPosts();
     });
   }
 
@@ -67,4 +71,26 @@ class PostsController extends GetxController with StateMixin {
 
   Future<void> goToCreatePostPage() async =>
       await Get.toNamed(Routes.createPostPage);
+
+  Future<void> _insertPosts() async {
+    for (Post post in posts) {
+      await insertPostUseCase(
+        InsertPostParams(
+            id: post.id!,
+            userId: post.userId,
+            title: post.title,
+            body: post.body),
+      );
+    }
+  }
+
+  Future<void> _getLocalPosts() async {
+    final result = await getLocalPostsUseCase(NoParamsGetLocalPosts());
+
+    //TODO ver excepciones
+    result.fold((l) => null, (r) {
+      posts = r;
+      _filteredPosts.value = List.from(r);
+    });
+  }
 }
